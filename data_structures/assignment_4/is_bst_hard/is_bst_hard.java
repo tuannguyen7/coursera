1be6c2502bcf2cccfead56b72da8035b35852cf9
tuannguyen7 <tuanna.4284@gmail.com>
2019-04-12 17:44:56 +0700

    try assiment 4, ex3

--- data_structures/assignment_4/is_bst_hard/is_bst_hard.java
+++ data_structures/assignment_4/is_bst_hard/is_bst_hard.java
@@ -15,18 +15,23 @@ public class is_bst_hard {
                 tok = new StringTokenizer(in.readLine());
             return tok.nextToken();
         }
+
         int nextInt() throws IOException {
             return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
         }
     }
 
     public class IsBST {
         class Node {
-            int key;
+            long key;
             int left;
             int right;
 
-            Node(int key, int left, int right) {
+            Node(long key, int left, int right) {
                 this.left = left;
                 this.right = right;
                 this.key = key;
@@ -35,19 +40,64 @@ public class is_bst_hard {
 
         int nodes;
         Node[] tree;
+        long curMax = Long.MIN_VALUE;
+        long curLeftMin = Long.MAX_VALUE;
+        long curMIn = Long.MAX_VALUE;
 
         void read() throws IOException {
             FastScanner in = new FastScanner();
             nodes = in.nextInt();
             tree = new Node[nodes];
             for (int i = 0; i < nodes; i++) {
-                tree[i] = new Node(in.nextInt(), in.nextInt(), in.nextInt());
+                tree[i] = new Node(in.nextLong(), in.nextInt(), in.nextInt());
             }
         }
 
         boolean isBinarySearchTree() {
-          // Implement correct algorithm here
-          return true;
+            // Implement correct algorithm here
+            if (tree.length == 0)
+                return true;
+            Node root = tree[0];
+            return inOrder(root, true);
+        }
+
+        long findMax(Node n) {
+            if (n.right != -1)
+                return findMax(tree[n.right]);
+            return n.key;
+        }
+
+        long findMin(Node n) {
+            if (n.left != -1)
+                return findMin(tree[n.left]);
+            return n.key;
+        }
+
+        boolean inOrder(Node n, boolean fromLeft) {
+            boolean leftRes = true;
+            if (n.left != -1) {
+                long maxLeft = findMax(tree[n.left]);
+                if (n.key <= maxLeft) {
+                    return false;
+                }
+                leftRes = inOrder(tree[n.left], fromLeft);
+            }
+            if (!leftRes)
+                return false;
+            // curMax = n.key;
+            if (n.right != -1) {
+//              boolean rightRes = inOrder(tree[n.right], false);
+                long minRight = findMin(tree[n.right]);
+                if (n.key > minRight || (n.key == minRight && tree[n.right].key != minRight)) {
+                    return false;
+                }
+                return inOrder(tree[n.right], false);
+            }
+            return true;
+        }
+
+        public boolean solve() {
+            return isBinarySearchTree();
         }
     }
 
@@ -61,9 +111,11 @@ public class is_bst_hard {
             }
         }, "1", 1 << 26).start();
     }
+
     public void run() throws IOException {
         IsBST tree = new IsBST();
         tree.read();
+        ArrayList<String> a = new ArrayList<>();
         if (tree.solve()) {
             System.out.println("CORRECT");
         } else {
